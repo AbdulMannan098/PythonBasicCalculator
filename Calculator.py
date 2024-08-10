aede2103371dd6bd07efbb9c0cbdc77dc3095864
AbdulMannan098 <124061332+AbdulMannan098@users.noreply.github.com>
2024-08-10 16:22:18 +0500

    Basic Calculator PIAIC

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+num1:float=float(input("Enter First Number: "))
+num2:float=float(input("Enter Second Number: "))
+operation:str=input("Enter the operation (+, -, *, /):")
+result:float
+if operation == '+':
+    result=num1 + num2
+elif operation == '-':
+    result=num1 - num2
+elif operation == '*':
+    result=num1 * num2
+elif operation == '/':
+    result=num1 / num2
+else:
+    print("Invalid operation")
+
+# Displaying the result
+print(f"The result of {num1} {operation} {num2} is {result}.")
